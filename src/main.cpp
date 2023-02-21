4a6c77dce8671447f1ae96cd0958a94f32b58c2c
Christoph Ellrich <christoph.ellrich@gmail.com>
2023-02-21 13:04:57 +0100

    added score and field reset

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,9 @@ Paddle *leftPaddle;
 Paddle *rightPaddle;
 Physics physics;
 
+int scoreLeft = 0;
+int scoreRight = 0;
+
 void setup()
 {
   Serial.begin(57600);
@@ -31,6 +34,31 @@ void setup()
   physics = Physics();
   ball->setVelocity(Vector(1, 2));
   physics.init(ball, leftPaddle, rightPaddle, display);
+}
+
+void drawScore() {
+  // todo draw either numbers or maybe points to indicate the current scores
+}
+
+void resetField() {
+  ball->setPosition(Vector(15.5,15.5));
+  int startDir = 0;
+  if (scoreLeft > scoreRight) {
+    startDir = 1;
+  } else if (scoreLeft < scoreRight)
+  {
+    startDir = -1;
+  } else {
+    startDir = random(-1, 1);
+  }
+  
+  ball->setVelocity(Vector(startDir, 0));
+  leftPaddle->setPosition(Vector(0,0));
+  rightPaddle->setPosition(Vector(0,31));
+  leftPaddle->setVelocity(Vector(0,0));
+  rightPaddle->setVelocity(Vector(0,0));
+  display->resetMatrix();
+  drawScore();
 }
 
 void loop()
@@ -72,11 +100,17 @@ void loop()
 
     // calculate game state
     String gameState = physics.update(ball, leftPaddle, rightPaddle, display);
-    if (gameState != "")
-    {  
-      ball->setPosition(Vector(15,15));
-      ball->setVelocity(Vector(1, 2));
-      Serial.println(gameState);
+    if (gameState == "leftHit")
+    { 
+      scoreRight += 1,
+      resetField();
+      Serial.println("right scored");
+    }
+    else if (gameState == "righttHit")
+    {
+      scoreLeft += 1,
+      resetField();
+      Serial.println("left scored");
     }
     lastFrameTime = thisFrameTime;
   }
